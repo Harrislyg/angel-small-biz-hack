e0bcca839b10f8f3bdf48821d0cb5df6a0865a9e
HarrisLeow <leow_harris@hotmail.sg>
2016-08-27 20:51:26 -0700

    fix duplicate update in app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react'
 import Header from './components/Header.jsx';
-import Transaction from './components/Transaction.jsx';
 import { render } from 'react-dom'
 
 // Load external modules

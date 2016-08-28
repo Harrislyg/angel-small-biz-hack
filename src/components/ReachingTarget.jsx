15eff4ad758b74263ecbb8f79367b8b751a686a2
Mai Anh Vu <me@maianhvu.com>
2016-08-28 10:39:57 -0700

    resolve merge conflict

--- src/components/ReachingTarget.jsx
+++ src/components/ReachingTarget.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Header from './Header.jsx'
-import Line from './Line.jsx'
-import Bar from './Bar.jsx'
-import BarChart2 from './BarChart2.jsx'
-import Slider from './Slider.jsx'
-import Goals from './Goals.jsx'
-import Pie from './Pie.jsx'
+// import Header from './Header.jsx'
+// import Line from './Line.jsx'
+// import Bar from './Bar.jsx'
+// import BarChart2 from './BarChart2.jsx'
+// import Slider from './Slider.jsx'
+// import Goals from './Goals.jsx'
+// import Pie from './Pie.jsx'
 
 class ReachingTarget extends React.Component {
   constructor(props) {

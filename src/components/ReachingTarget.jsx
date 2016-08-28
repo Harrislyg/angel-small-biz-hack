aa94a27081f2fae22514ae56b8e61554bd3e41c0
ravic1995 <ravi.chandak901@gmail.com>
2016-08-28 11:35:20 -0700

    hi

--- src/components/ReachingTarget.jsx
+++ src/components/ReachingTarget.jsx
@@ -1,9 +1,47 @@
 import React, { Component } from 'react'
+const PieChart = require('react-chartjs').Pie;
+import { ProgressBar } from 'react-bootstrap';
 
 class ReachingTarget extends Component {
   render() {
+  	const data=
+        [{value:80.00,
+        	color:'#F7464A',
+        	highlight:'#FF5A5E',
+        	label:'Reinvest',
+        },
+        {value:20.00,
+       	color:'#46BFBD',
+        highlight:'#5AD3D1',
+        label:'Remaining Balance'},
+        ];
+
+    const options =  {
+    	responsive: true,
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
+            }]
+        }
+    }
+
+
     return (
-      <div className="reaching-target"></div>
+      <div >
+      <div>
+      <ProgressBar striped bsStyle="success" now={20} />
+      </div>
+      <p> To achieve your target </p>
+      <div>
+      <p> You need to reinvest 20% of your monthly income </p>
+      <PieChart data={data} options={options} />
+      </div>
+
+      <p> Automate your reinvest</p>
+      <button type="button">Reinvest </button>
+      </div>
+
     )
   }
 }

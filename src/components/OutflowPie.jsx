625b476b72a59749e70f6d76f68c60de9ab0c134
ravic1995 <ravi.chandak901@gmail.com>
2016-08-28 12:08:05 -0700

    Yeah bitches we there

---
+++ src/components/OutflowPie.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+// import Plotly from 'react-plotlyjs';
+const PieChart = require('react-chartjs').Pie;
+
+class InflowOutflow extends React.Component {
+
+	render(){
+
+	 const index = this.props.month-8;
+        const datap = this.props.data;
+        console.log(datap);
+    const data=
+        [{value:datap[index][0],
+            color:'#F7464A',
+            highlight:'#FF5A5E',
+            label:'Inventory',
+        },
+        {value:datap[index][1],
+        color:'#46BFBD',
+        highlight:'#5AD3D1',
+        label:'Payroll'},
+        {value:datap[index][2],
+            color:'#FDB45C',
+            highlight:'#FFC870',
+            label:'Rent and Utilities '},
+        {value:datap[index][3],
+            color:'#637b85',
+            highlight:'#FFC870',
+            label:'Investing and Financing'},
+            {value:datap[index][4],
+            color:'#c62f29',
+            highlight:'#FFC870',
+            label:'Miscellaneous'}
+
+
+        ];
+
+
+    const options =  {
+    	responsive: true,
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
+            }]
+        }
+    }
+
+
+		return (
+			<div>
+			<PieChart data={data} options={options} />
+			</div>
+		);
+	}
+}
+
+export default InflowOutflow;
--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -5,6 +5,9 @@ import Slider from './Slider.jsx'
 import Header from './Header.jsx'
 import Goals from './Goals.jsx'
 import Line from './Line.jsx'
+import InflowPie from './InflowPie.jsx'
+import OutflowPie from './OutflowPie.jsx'
+
 
 import transactionData from 'json!../data/filtered.json'
 
@@ -41,6 +44,11 @@ class Transaction extends React.Component {
   }
 
   render () {
+    const {
+      inflow,
+      outflow
+    } = this.props;
+
     console.log('Transaction', transactionData)
     console.log(this.state.currentMonth)
     return (
@@ -48,11 +56,30 @@ class Transaction extends React.Component {
         <div>
         <Slider onClick={this.handleChange} currentMonth={this.state.currentMonth} />
         </div>
+
+        <InflowPie data={inflow} month={this.state.currentMonth} />
         <Inflow onSelect={this.handleSelect} category={this.state.category} inflowData={transactionData} sliderDate={this.state.currentMonth}/>
+
+        <OutflowPie data={outflow} month={this.state.currentMonth} />
         <Outflow onOutflow={this.handleOutflow} categoryOutflow={this.state.categoryOutflow} outflowData={transactionData} sliderDate={this.state.currentMonth}/>
       </div>
     )
   }
 }
-
+Transaction.defaultProps={
+  outflow: [
+  [42,20,19,13,6],
+  [27,19,24,13,7],
+  [46,21,22,8,3],
+  [48,18,21,7,6],
+  [21,19,20,13,7]
+  ],
+  inflow: [
+    [33,18,49],
+    [28,22,40],
+    [27,16,37],
+    [52,25,23],
+    [59,15,28]
+  ]
+}
 module.exports = Transaction

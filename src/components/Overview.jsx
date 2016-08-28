6ce55c2069e951dec647f2383fae10e6419e5600
Mai Anh Vu <me@maianhvu.com>
2016-08-27 23:49:19 -0700

    adjust styling for Overview

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
+import CurrencyFormatter from '../helpers/CurrencyFormatter'
 
 class Overview extends Component {
   render() {
     let statsItems = [
       { title: 'Inflows', currency: 'US$', amount: 3000, identifier: 'inflows' },
-      { title: 'Outflows', currency: 'US$', amount: 3000, identifier: 'outflows' },
-      { title: 'Your Balance', currency: 'US$', amount: 3000, identifier: 'your_balance' },
-      { title: 'Your Target', currency: 'US$', amount: 3000, identifier: 'your_target' },
+      { title: 'Outflows', currency: 'US$', amount: 4000, identifier: 'outflows' },
+      { title: 'Your Balance', currency: 'US$', amount: 5000, identifier: 'your_balance' },
+      { title: 'Your Target', currency: 'US$', amount: 6000, identifier: 'your_target' },
     ]
 
     let statsNodes = statsItems.map((statsData, index) => {
@@ -14,7 +15,7 @@ class Overview extends Component {
         <div className="overview__stats--item" key={index} id={`overview__stats--${statsData.identifier}`}>
           <div className="overview__stats--title">{statsData.title}</div>
           <div className="overview__stats--currency">{statsData.currency}</div>
-          <div className="overview__stats--amount">{statsData.amount}</div>
+          <div className="overview__stats--amount">{CurrencyFormatter.formatCurrency(statsData.amount)}</div>
         </div>
       );
     });

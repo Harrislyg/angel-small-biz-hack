b89cbf348748ccd201bd94089b3a311147806931
Timothy Chong <timothychong@Justins-MacBook-Pro-3.local>
2016-08-27 19:24:47 -0700

    moved component into src

---
+++ src/components/analytics.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Aanlytics from '../components/analytics.jsx'
+
+class Aanlytics extends React.Component {
+  render() {
+    return (
+    	<Analytics/>
+    	<p>Amazing</p>
+    );
+  }
+}
+
+export default Aanlytics;

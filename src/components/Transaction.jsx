9b4cb1794d082d4fe727376fbd66621dc838daa3
HarrisLeow <leow_harris@hotmail.sg>
2016-08-27 20:44:53 -0700

    fix merge conflict in package.json

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,10 +1,12 @@
 var React = require('react')
 var Inflow = require('./Inflow.jsx')
 var Outflow = require('./Outflow.jsx')
+import transactionData from 'json!../data/transaction.json'
 
 class Transaction extends React.Component {
 
   render () {
+    console.log(transactionData)
     return (
       <div>
         <Inflow />
